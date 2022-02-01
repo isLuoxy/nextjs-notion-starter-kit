885b698b0bf3f1ff8a976ef93148231730b76a6e
isLuoxy <33096627+isLuoxy@users.noreply.github.com>
2022-02-01 21:47:47 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '4ed30d0f3f2d4ff888e675cd6bc5cf22',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '爱 「世界与和平」',
+  domain: 'https://www.notion.so/4ed30d0f3f2d4ff888e675cd6bc5cf22',
+  author: 'Solomon Luo',
 
   // open graph metadata (optional)
   description: 'Example site description',
